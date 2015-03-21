09b5d8bd862791a36f498593f34a34ffac724bff
shelly8525 <shelly8525@yahoo.com.tw>
2015-03-22 00:06:14 +0800

    Create no-arg constructor accessor mutator method and the two methods

--- src/Account.java
+++ src/Account.java
@@ -1,15 +1,54 @@
+import java.util,Scanner;
+
 public class Account {
 	private int id = 0;
 	private double blance = 0;
 	private double annualInterestRate = 0;
 	private java.util.Date dateCreated;
-
-	Account(){
-		id = 0;
+	input Scanner = new input(System.in);
+	
+	public Account(int id,double blance){
+		this.id = id;
+		this.blance = blance;
 	}
 	
-	new Account(id = 0);
-	new Account(blance = 0);
+	public int getId(){
+		return id;
+	}
 	
+	public double getBlance(){
+		return blance;
+	}
 	
+	public double getAnnualInterestRate(){
+		return annualInterestRate;
+	}
+	
+	public java.util.Date getDateCreated(){
+		return dateCreated;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public void setBlance(double blance){
+		this.blance = blance;
+	}
+	
+	public void setAnnualInterestRate(){
+		this.annualInterestRate = annualInterestRate;
+	}
+	
+	public double getMonthlyInterestRate(){
+		return annualInterestRate/12/100;
+	}
+	
+	public double getMonthlyInterest(){
+		return blance*(annualInterestRate/12/100);
+	}
+	
+	public double withdraw(){
+		
+	}
 }
