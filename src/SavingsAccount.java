c4d839dda1b70cb389592a41a6192458e09c5e72
shelly8525 <shelly8525@yahoo.com.tw>
2015-03-23 00:33:16 +0800

    建立super建構子

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,10 +1,13 @@
 
 public class SavingsAccount extends Account{
+	public SavingsAccount(int id, double blance) {
+		super(id, blance);
+	}
 	public static void main(String[] args){
 		
 	}
 		@Override
 		public String toString(){
-			 return "Savings Account: ID: " + getId()+ "Balance: " + getBalance() + "Rate: " + getAnnualInterestRate();
+			 return "Savings Account: ID: " + getId()+ "Balance: " + getBlance() + "Rate: " + getAnnualInterestRate();
 		}
 }
