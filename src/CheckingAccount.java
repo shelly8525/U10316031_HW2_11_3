597401df893330b755a556bf0042f3d24c40f9dd
shelly8525 <shelly8525@yahoo.com.tw>
2015-03-23 00:39:35 +0800

    註解CheckingAccount 和SavingsAccount class

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,11 +1,13 @@
 
 public class CheckingAccount extends Account{
+	//create super constructor
 	public CheckingAccount(int id, double blance) {
 		super(id, blance);
 	}
 	public static void main(String[] args){
-		
+		//main method
 	}
+	//Inherit Account class method
 	@Override
 	public String toString(){
 		return "Checking Account: ID: " + getId() + "Balance: " + getBlance() + "Rate: " + getAnnualInterestRate();
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,11 +1,14 @@
 
 public class SavingsAccount extends Account{
+	//create super constructor
 	public SavingsAccount(int id, double blance) {
 		super(id, blance);
 	}
 	public static void main(String[] args){
+		//main method
 		
 	}
+		//Inherit Account class method
 		@Override
 		public String toString(){
 			 return "Savings Account: ID: " + getId()+ "Balance: " + getBlance() + "Rate: " + getAnnualInterestRate();
